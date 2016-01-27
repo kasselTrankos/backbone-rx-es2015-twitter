1155380ffa5da3ff26f05d1de18b69e91424d7a7
kasselTrankos <alvaro.touzon@gmail.com>
2016-01-27 19:10:06 +0100

    homeView started

---
+++ src/app/home/view.js
@@ -0,0 +1,21 @@
+var Backbone = require('Backbone');
+
+var homeView = Backbone.View.extend({
+
+  tagName: 'div',
+
+  id:'form',
+
+  events: {
+  },
+
+  initialize: function() {
+    console.log('init application');
+  },
+
+  render: function() {
+
+  }
+
+});
+module.exports  = homeView;
--- src/app/main.js
+++ src/app/main.js
@@ -1,18 +1,3 @@
 'use strict';
-var backbone = require('backbone')
-// require another module relative to current module
-var foo = ['ab', 'bc', 'nmk the who molo to say it reloaddd hhhh no hay reis;'];
-/*
-// require a vanilla node module managed by npm
-var _ = require('underscore');
-
-_.forEach(foo(), function (f) {
-  // create a <p> tag for each f and add to html body
-  var p = document.createElement('p');
-  p.innerHTML = f;
-  if (document.body != null) {
-    document.body.appendChild(p);
-  }
-
-});
-*/
+var homeView = require('./home/view');
+var home = new homeView();
