6dfffa8d41f43838bb42e9172ddb32f00b7c877a
kasselTrankos <alvaro.touzon@gmail.com>
2016-01-27 22:09:09 +0100

    with virtual dom

--- src/app/home/view.js
+++ src/app/home/view.js
@@ -1,19 +1,40 @@
 var Backbone = require('Backbone');
+var h = require('virtual-dom/h');
+var createRootNode = require('virtual-dom/create-element');
+var VNode = require('virtual-dom/vnode/vnode');
+var VText = require('virtual-dom/vnode/vtext');
 
 var homeView = Backbone.View.extend({
 
   tagName: 'div',
 
-  id:'form',
+  el:'#form',
 
   events: {
   },
 
   initialize: function() {
     console.log('init application');
+    this.render();
   },
 
   render: function() {
+    var count = 0;      // We need some app data. Here we just store a count.
+
+    console.log('render it please');
+    var form = new VNode('form', {
+      action: '/account',
+      method:'POST',
+      className:'row'
+    });
+    var node =  new VNode('p', {
+        className: "greeting"
+    }, [
+        new VText("Hello " + String('vera esta encasa!!!'))
+    ]);
+    this.$el.append(createRootNode(node));
+    
+    return this;
 
   }
 
