df9a4cd1276de3a76470e58d677b56f4ef3b9b9b
kasselTrankos <alvaro.touzon@gmail.com>
2016-02-14 14:04:08 +0100

    little change

---
+++ src/app/tweet/views/Pages.js
@@ -0,0 +1,54 @@
+import {View} from 'backbone';
+import {ListPagesView} from './../tpl';
+import Route from './../../route';
+import Rx from 'rx';
+
+export default class List extends View{
+  constructor(options){
+    super(options);
+    this.page = options.page;
+    this.pagesShown = options.pagesShown;
+    this.account = options.account;
+    this.route = options.route;
+    this.listPages = ListPagesView(this.$el, this.account, this.pagesShown);
+  }
+  events(){
+
+    /*return {
+      'click .pagination a': 'gotoPage'
+    };*/
+  }
+  observables(){
+    const Route = this.route;
+    const pagination = Rx.Observable.fromEvent(
+      document.querySelectorAll('.pagination a'), 'click', false)
+      .do((e)=>{e.preventDefault();})
+      .pluck('target', 'href')
+      .subscribe((href)=>{
+        Route.navigate(`/${_.takeRight(href.split('/'), 2).join('/')}`, {trigger:true});
+      });
+  }
+  tagName(){
+    return 'div';
+  }
+  el() {
+    return '#pageTweets';
+  }
+  ///used by events in backbone
+  gotoPage(e){
+    e.preventDefault();
+    this.route.navigate(e.currentTarget.getAttribute('href'), {trigger:true});
+
+    return false;
+  }
+  initialize(){
+
+  }
+  renderPagesTweets(size, currpage=1){
+    this.listPages(size, currpage);
+    this.observables();
+  }
+  renderListTweets(){
+
+  }
+}
