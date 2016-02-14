d37edf6e492bf9b841eb3d73edff0c850996d867
kasselTrankos <alvaro.touzon@gmail.com>
2016-02-14 13:58:18 +0100

    added Rx in pagination link a

--- src/app/tweet/views/Pages.js
+++ src/app/tweet/views/Pages.js
@@ -1,6 +1,7 @@
 import {View} from 'backbone';
 import {ListPagesView} from './../tpl';
 import Route from './../../route';
+import Rx from 'rx';
 
 export default class List extends View{
   constructor(options){
@@ -10,12 +11,24 @@ export default class List extends View{
     this.account = options.account;
     this.route = options.route;
     this.listPages = ListPagesView(this.$el, this.account, this.pagesShown);
+
     //this.on('GOTO_PAGE', this.renderPagesTweets, this);
   }
   events(){
-    return {
+
+    /*return {
       'click .pagination a': 'gotoPage'
-    };
+    };*/
+  }
+  observables(){
+    const Route = this.route;
+    const pagination = Rx.Observable.fromEvent(
+      document.querySelectorAll('.pagination a'), 'click', false)
+      .do((e)=>{e.preventDefault();})
+      .pluck('target', 'href')
+      .subscribe((href)=>{
+        Route.navigate(`/${_.drop(href.split('/'), 3).join('/')}`, {trigger:true});
+      });
   }
   tagName(){
     return 'div';
@@ -23,16 +36,19 @@ export default class List extends View{
   el() {
     return '#pageTweets';
   }
+  ///used by events in backbone
   gotoPage(e){
     e.preventDefault();
     this.route.navigate(e.currentTarget.getAttribute('href'), {trigger:true});
+
     return false;
   }
   initialize(){
 
   }
   renderPagesTweets(size, currpage=1){
-    this.listPages(size, currpage)
+    this.listPages(size, currpage);
+    this.observables();
   }
   renderListTweets(){
 
